73c940f4292619316718826f35ea79622d143085
Felipe Acquati <felipe_acq@yahoo.com.br>
2017-10-22 23:04:34 -0200

    fix: Correction of some bugs and information added in the readme

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # Memory Game Project
 
-### The game randomly shuffles the cards. A user wins once all cards have successfully been matched.
+The game randomly shuffles the cards. A user wins once all cards have successfully been matched.
 
-## Instructions
+## How it was created and libraries used
+The game was created using the following languages: HTML, CSS and Javascript.
+
+### Libraries
+* Bootstrap v3.3.7
+* jQuery v3.2.1
+
+### Compatible browsers
+* Google Chrome
+* Firefox
+
+## How the game is played.
 
 Choose two cards, if they are the same they remain face-up, otherwise they will turn down again.  
 The game ends when all pairs of matching cards are found.  
 
 ### Stars Rating
+The fewer moves you make to find all pairs of cards, the better your **Star Rating** will be.
 
-3 Stars - up to 29 moves.  
-2 Stars - up to 59 moves.  
-1 Star - more than 60 moves.  
+* 3 Stars - up to 20 moves.  
+* 2 Stars - up to 40 moves.  
+* 1 Star - more than 40 moves.  
 
 ### Restart Button
 
 A restart button allows the player to reset the game board, the timer, and the star rating.
-
--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 /*
  * Create a list that holds all of your cards
  */
-var diamond = '<li class="card"><i class="fa fa-diamond"></i></li>';
-var paperPlane = '<li class="card"><i class="fa fa-paper-plane-o"></i></li>';
-var anchor = '<li class="card"><i class="fa fa-anchor"></i></li>';
-var bolt = '<li class="card"><i class="fa fa-bolt"></i></li>';
-var cube = '<li class="card"><i class="fa fa-cube"></i></li>';
-var leaf = '<li class="card"><i class="fa fa-leaf"></i></li>';
-var bycicle = '<li class="card"><i class="fa fa-bicycle"></i></li>';
-var bomb = '<li class="card"><i class="fa fa-bomb"></i></li>';
+let diamond = '<li class="card"><i class="fa fa-diamond"></i></li>';
+let paperPlane = '<li class="card"><i class="fa fa-paper-plane-o"></i></li>';
+let anchor = '<li class="card"><i class="fa fa-anchor"></i></li>';
+let bolt = '<li class="card"><i class="fa fa-bolt"></i></li>';
+let cube = '<li class="card"><i class="fa fa-cube"></i></li>';
+let leaf = '<li class="card"><i class="fa fa-leaf"></i></li>';
+let bycicle = '<li class="card"><i class="fa fa-bicycle"></i></li>';
+let bomb = '<li class="card"><i class="fa fa-bomb"></i></li>';
 
-var allCards = [diamond,diamond,paperPlane,paperPlane,anchor,anchor,bolt,bolt,
+let allCards = [diamond,diamond,paperPlane,paperPlane,anchor,anchor,bolt,bolt,
                 cube,cube,leaf,leaf,bycicle,bycicle,bomb,bomb];
-var deckId;
-var listOpenCards = [];
-var blockClick = false;
-var movesNumber = 0;
-var cardsMatched = 0;
-var counterTime = 0;
-var myInterval;
+let deckId;
+let listOpenCards = [];
+let blockClick = false;
+let movesNumber = 0;
+let cardsMatched = 0;
+let counterTime = 0;
+let myInterval;
+let starsNumber = 3;
 
 /*
  * Display the cards on the page
@@ -29,7 +30,7 @@ var myInterval;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -42,20 +43,20 @@ function shuffle(array) {
     return array;
 }
 
-// Shuffle the list of cards
+// @description Shuffle the list of cards
 shuffle(allCards);
 
-// Loop through each card and create its HTML
+// @description Loop through each card and create its HTML
 deckId = $('#deck');
 
 function cardHtml(array) {
-    for (var i=0 ; i<array.length; i++){
+    for (let i=0 ; i<array.length; i++){
         deckId.append(array[i]);
     }
     //return console.log(i);
 }
 
-// post cards on the deck
+// @description post cards on the deck
 cardHtml(allCards);
 
 /*
@@ -69,6 +70,7 @@ cardHtml(allCards);
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+// @description restart the game
 function restart() {
     listOpenCards = [];
     blockClick = false;
@@ -85,19 +87,25 @@ function restart() {
     cardHtml(allCards);
     $('.moves').text(movesNumber);
     $('.stars li').children().addClass('rating');
+    starsNumber = 3;
     startTime();
-
-
 }
 
-//Event listener for a restart button
+// @description Event listener for a restart button
 $( '.restart' ).on( 'click', 'i', function( evt ) {
-    var clicked = $( evt.target );
+    let clicked = $( evt.target );
     restart();
-    console.log(clicked);
+    //console.log(clicked);
 });
 
- // Time Elapsed - Clock
+// @description Event listener for a play again button
+$( '.modal-footer' ).on( 'click', '#play-again', function( evt ) {
+    let clicked = $( evt.target );
+    restart();
+    console.log("Play again btn: "+clicked);
+});
+
+ // @description Time Elapsed - Clock
 function startTime() {
      myInterval = setInterval(function () {
          ++counterTime;
@@ -107,53 +115,62 @@ function startTime() {
 
 startTime();
 
- // Event listener for a card
+ // @description Event listener for a card
  $( '#deck' ).on( 'click', 'li', function( evt ) {
-     var clickedCard = $( evt.target );
+     let clickedCard = $( evt.target );
 
      if (!blockClick) {
-        openShow(clickedCard);
+         clickedCard.addClass('disable-click');
+         openShow(clickedCard);
      }
      //console.log(evt);
  });
 
-//display the card's symbol
+// @description display the card's symbol
 function openShow(card){
-    card.addClass('open show');
+    card.addClass('open show2');
     openCards(card);
 }
 
-// add card open to listOpenCards
+// @description add card open to listOpenCards
 function openCards(card){
-    var x = card.children().attr('class');
+    let x = card.children().attr('class');
     console.log(x);
+    // block 'click' in open card
+    //card.off("click");
     //add card to listOpenCards
     listOpenCards.push(card);
     console.log(listOpenCards);
-    movesNumber++;
-    starRating();
-    $('.moves').text(movesNumber);
+
     checkCards();
 }
 
+// @description calculates the star rating based on the number of moves
 function starRating() {
-    if ( movesNumber === 30 ) {
+    if ( movesNumber === 20 ) {
         $('.stars li:nth-child(1)').children().removeClass('rating');
+        starsNumber--;
     }
-    if ( movesNumber === 60 ) {
+    if ( movesNumber === 40 ) {
         $('.stars li:nth-child(2)').children().removeClass('rating');
+        starsNumber--;
     }
 }
 
-// check to see if the two cards match
+// @description check to see if the two cards match
 function checkCards(){
     if (listOpenCards.length === 2) {
-        var classCard1 = listOpenCards[0].children().attr('class');
-        var classCard2 = listOpenCards[1].children().attr('class');
+        // add +1 to Moves - update star rating
+        movesNumber++;
+        starRating();
+        $('.moves').text(movesNumber);
+        //
+        let classCard1 = listOpenCards[0].children().attr('class');
+        let classCard2 = listOpenCards[1].children().attr('class');
         //console.log(classCard1,classCard2);
         // block to choice other cards
         blockClick = true;
-
+        // compare if the cards are the same
         if (classCard1 === classCard2){
             lockCards();
         }
@@ -163,11 +180,11 @@ function checkCards(){
     }
 }
 
-//cards do match, lock the cards in the open position
+// @description cards do match, lock the cards in the open position
 function lockCards() {
 
-    listOpenCards[0].removeClass('open show').addClass('match');
-    listOpenCards[1].removeClass('open show').addClass('match');
+    listOpenCards[0].removeClass('open show2').addClass('match');
+    listOpenCards[1].removeClass('open show2').addClass('match');
     listOpenCards[0].animate({
             height: '+=10px',
             width: '+=10px'
@@ -202,23 +219,27 @@ function lockCards() {
                 blockClick = false;
                 cardsMatched++;
                 if (cardsMatched == 8){
-
-                    window.confirm("You Win\n"+
-                                    "Moves: "+movesNumber+
-                                    "\nTime elapsed: "+counterTime+" seconds");
-                    restart();
+                    $('#myModal').modal('toggle');
+                    $('#moves-number').text(movesNumber);
+                    $('#counter-time').text(counterTime);
+                    $('#stars-number').text(starsNumber);
+                    // window.confirm("You Win\n"+
+                    //                 "Moves: "+movesNumber+
+                    //                 "\nTime elapsed: "+counterTime+" seconds");
+                    //restart();
                 }
 
         });
 
     }
 
-//cards do not match
+// @description cards do not match
 function flipCards() {
     cardEffect();
 
 }
 
+// @description generates the effect fade Out and In (jQuery)
 function cardEffect() {
     listOpenCards[0].delay(300).fadeOut("slow").fadeIn("slow");
     listOpenCards[1].delay(300).fadeOut("slow").fadeIn("slow", function() {
@@ -227,14 +248,18 @@ function cardEffect() {
     //hideCards();
 }
 
+// @description Hide the cards
 function hideCards() {
     console.log(listOpenCards);
-    listOpenCards[0].removeClass("open show");
-    listOpenCards[1].removeClass("open show");
+    listOpenCards[0].removeClass("open show2");
+    listOpenCards[1].removeClass("open show2");
+    listOpenCards[0].removeClass("disable-click");
+    listOpenCards[1].removeClass("disable-click");
     removeCardsFromList();
 
 }
 
+// @description Removes the cards from the list that stores them
 function removeCardsFromList() {
     listOpenCards.shift();
     listOpenCards.shift();
@@ -242,7 +267,3 @@ function removeCardsFromList() {
     // unblock to choice other cards
     blockClick = false;
 }
-
-
-// to stop the counter
-// clearInterval(myInterval);
